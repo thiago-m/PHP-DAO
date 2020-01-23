ffe01ddb960bb9c2c3b90748305ba8831dbb6ddd
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-01-23 19:49:36 -0300

    Criacao da funcionalidade de cadastrar usuario

--- index.php
+++ index.php
@@ -14,9 +14,15 @@
     // $search = Usuario::search('Ma');
     // echo json_encode($search);
 
-    ## Carregar usuario por login e senha
+    ### Carregar usuario por login e senha
     // $usuario = new Usuario();
     // $usuario->login('jose', '0123');
     // echo $usuario;
+
+
+    ### Cadastro de usuario
+    $user = new Usuario('Larissa', 'teste');
+    $user->insert();
+    echo $user;
 
 ?>
