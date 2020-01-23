0e8291f778b6b686f61292d67df9237eb50a19a6
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-01-23 19:03:07 -0300

    Criando a class Usuario

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
     require_once("config.php");
-    $sql = new Sql();
-    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+    
+    $root = new Usuario();
+    $root->loadById(1);
 
-    echo json_encode($usuarios);
+    echo $root;
 ?>
