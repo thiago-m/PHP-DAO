5fd2c7191c5d14232e6780e6132ce756986a7ca2
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2020-01-23 19:24:34 -0300

    Cria;'ao das funcionalidades de listar usuarios, pesquisar por login e pesquisar por login e senha

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php 
     require_once("config.php");
     
-    $root = new Usuario();
-    $root->loadById(1);
+    ### Carrega 1 usuario
+    // $root = new Usuario();
+    // $root->loadById(1);
+    //echo $root;
 
-    echo $root;
+    ### Carrega uma lista de usuarios
+    // $list = Usuario::getList();
+    // echo json_encode($list);
+
+    ### Pesquisar usuarios
+    // $search = Usuario::search('Ma');
+    // echo json_encode($search);
+
+    ## Carregar usuario por login e senha
+    $usuario = new Usuario();
+    $usuario->login('jose', '0123');
+    echo $usuario;
+
 ?>
